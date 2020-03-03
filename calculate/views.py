47e9b2c2eec23665f4d77fe76ca6113ad0d7f90b
jonghh <jonghhhhhh@gmail.com>
2020-03-03 16:31:45 +0900

    noun analysis

---
+++ calculate/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.urls import reverse
+from .models import *
+from konlpy.tag import Twitter
+from collections import Counter
+
+def calculate(request):
+    # return HttpResponse('<h1>hello</h1>')
+    result = ""
+    if request.method == "GET":
+        pass
+    elif request.method == "POST":
+        number1 = request.POST['number1']
+        tagger = Twitter()
+        nouns = tagger.nouns(number1)
+        count = Counter(nouns)
+        result = count.most_common(100)
+    else:
+            result = "nothing"
+    return render(request, 'calculate/calculate.html', {'result':result})
