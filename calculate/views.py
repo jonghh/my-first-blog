115990c22130bb2d3c20ad7dab25bb3f3b309f42
jonghh <jonghhhhhh@gmail.com>
2020-03-03 23:46:29 +0900

    nouns analysis

--- calculate/views.py
+++ calculate/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.urls import reverse
 from .models import *
-from konlpy.tag import Twitter
+from konlpy.tag import Kkma
 from collections import Counter
 
 def calculate(request):
@@ -12,7 +12,7 @@ def calculate(request):
         pass
     elif request.method == "POST":
         number1 = request.POST['number1']
-        tagger = Twitter()
+        tagger = Kkma()
         nouns = tagger.nouns(number1)
         count = Counter(nouns)
         result = count.most_common(100)
