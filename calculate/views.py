a0e8361f81dc64e7fc00a19e09fa165a69b28aad
jonghh <jonghhhhhh@gmail.com>
2020-03-04 14:20:25 +0900

    quote verb findings

--- calculate/views.py
+++ calculate/views.py
@@ -13,12 +13,20 @@ def calculate(request):
         pass
     elif request.method == "POST":
         number1 = request.POST['number1']
-        regex = re.compile("\w+[가이는은] ")
-        findings = regex.findall(number1)
-        result =[]
-        for word in findings:
-            word = re.sub('[가이은는] ', '', word)
-            result.append(word)
+## 직접인용 술어부 찾기
+        regex1 = re.compile('\"고 \w+[다].')
+        regex2 = re.compile('\"라고 \w+[다].')
+        findings1 = regex1.findall(number1)
+        findings2 = regex2.findall(number2)
+        result = findings1 + findings2
+## 주어 찾기
+        #regex = re.compile("\w+[가이는은] ")
+        #findings = regex.findall(number1)
+        #result =[]
+        #for word in findings:
+        #    word = re.sub('[가이은는] ', '', word)
+        #    result.append(word)
+## 명사 빈도 분석기
         # tagger = Kkma()
         # nouns = tagger.nouns(number1)
         # count = Counter(nouns)
