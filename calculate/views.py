db3f09c8f2d107ce9515ddce8fc9f3caee0eead2
jonghh <jonghhhhhh@gmail.com>
2020-03-04 13:15:07 +0900

    subject findings

--- calculate/views.py
+++ calculate/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.urls import reverse
 from .models import *
-from konlpy.tag import Kkma
+from re
+# from konlpy.tag import Kkma
 from collections import Counter
 
 def calculate(request):
@@ -12,10 +13,18 @@ def calculate(request):
         pass
     elif request.method == "POST":
         number1 = request.POST['number1']
+        regex = re.compile("\w+[가이는은] ")
+        findings = regex.findall(number1)
+        result =[]
+        for word in findings:
+            word = re.sub('[가이은는] ', '', word)
+            result.append(word)
+"""
         tagger = Kkma()
         nouns = tagger.nouns(number1)
         count = Counter(nouns)
         result = count.most_common(100)
+"""
     else:
             result = "nothing"
     return render(request, 'calculate/calculate.html', {'result':result})
